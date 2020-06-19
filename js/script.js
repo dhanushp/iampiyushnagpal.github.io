cb41fbd58bf7e4503999759e12b7bf949f535eb8
iampiyushnagpal <iampiyushnagpal@gmail.com>
2020-06-19 22:51:46 +0530

    Added Scroll to Top Button

--- js/script.js
+++ js/script.js
@@ -8,4 +8,23 @@ function expandMenu(){
 
 hamburger.addEventListener("click",expandMenu);
 
-//
+// Scroll to Top Function
+
+const scrollButton = document.getElementById("scroll__top__btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+   scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+} 
+
+scrollButton.addEventListener("click",topFunction);
